5890d75a7d5c29fa806ffc01a0632a47fab7f36a
Arthur Edelstein <arthuredelstein@gmail.com>
2015-07-14 12:36:20 -0700

    simple enhancements

--- init.el
+++ init.el
@@ -1,26 +1,28 @@
-(require 'package)
-(add-to-list 'package-archives
-  '("melpa-stable" . "http://melpa.milkbox.net/packages/") t)
-
-(package-initialize)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)
-
 ;; Always use spaces instead of tabs.
 (setq-default indent-tabs-mode nil)
 ;; Use 2 spaces per tab in JavaScript.
 (setq js-indent-level 2)
 ;; Turn column number mode on.
 (setq column-number-mode t)
+;; Make selection visible
+(transient-mark-mode)
+;; Better selection color
+(set-face-attribute 'region nil :background "#CCC")
+;; Use clojure-mode with .cljs files
+;(add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
+;; Don't add newlines to the end of files
+(setq require-final-newline nil)
+;; Set HTML default indentation
+(add-hook 'html-mode-hook
+          (lambda ()
+            ;; Default indentation to 1, but let SGML mode guess, too.
+            (set (make-local-variable 'sgml-basic-offset) 1)
+            (sgml-guess-indent)))
+;; Show trailing whitespace
+(setq-default show-trailing-whitespace t)
+(put 'downcase-region 'disabled nil)
+;; Activate recent files
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 30)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
