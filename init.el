c6c79143a8021d3244f32093c409cabcd2d046e7
Arthur Edelstein <arthuredelstein@gmail.com>
2016-04-07 19:41:27 -0700

    melpa stable

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+             '("melpa" . "http://stable.melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
