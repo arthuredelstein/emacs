73a9af741c39c34e2a600b5d3f95a75f585b821d
Arthur Edelstein <arthuredelstein@gmail.com>
2020-09-07 09:54:53 -0700

    use melpa stable

--- init.el
+++ init.el
@@ -1,12 +1,11 @@
 ;; use packages
-(setq package-list '(clojure-mode cider web-mode go-mode))
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://stable.melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+
+(setq package-list '(clojure-mode cider web-mode go-mode))
+
 (package-initialize)
 
 ; fetch the list of packages available
@@ -86,7 +85,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((js-indent-level . 2)))))
+ '(package-selected-packages '(go-mode web-mode cider clojure-mode))
+ '(safe-local-variable-values '((js-indent-level . 2))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
